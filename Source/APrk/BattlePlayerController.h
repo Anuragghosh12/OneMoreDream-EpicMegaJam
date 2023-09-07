04ec569e1afa2a82ac781971605ed83b6d750ec3
Anurag Ghosh <anuragghosh2013@gmail.com>
2023-09-07 23:21:27 +0530

    Added Turn Based GameMode(+UI)

---
+++ Source/APrk/BattlePlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BattlePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APRK_API ABattlePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
