149529bb98795edb8c5d242fdf8c4fe9afe16343
Anuragghosh12 <anuragghosh2013@gmail.com>
2023-09-10 10:09:50 +0530

    GameLoop Created, Enemy And Character Attacks Defined And Implemented Along With Turn Logic
    Implemented Gamemode for Turn Based Combat
    Also Implemented Enemy And Character Skill Set(+UI) Along With Turn Logic To Select Who Moves First

---
+++ Source/APrk/MyAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "MyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APRK_API AMyAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
